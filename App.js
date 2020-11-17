3e252e087834d329c20fb2948a2011323fe69ebf
Demi <72812729+just-demi@users.noreply.github.com>
2020-11-17 13:35:30 +0200

    created avatar component

--- App.js
+++ App.js
@@ -2,17 +2,27 @@ import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 
-import Homepage from './src/Homepage'
+import Homepage from './src/Homepage';
+import Avatar from './src/components/Avatar';
+import { render } from 'react-dom';
+import react from 'react';
+import Constants from 'expo-constants';
 
-export default function App() {
-  return <Homepage />;
+export default class App extends react.Component{
+  render(){
+  return (
+    <View style={styles.container}>
+    <Avatar initials={'FL'} size={35} backgroundColor={'teal'} />
+    </View>
+  );
+  }
+  //return <Homepage />;
 }
 
 const styles = StyleSheet.create({
   container: {
+    marginTop: Constants.StatusBarHeight,
     flex: 1,
     backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
   },
 });
